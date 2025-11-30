62303394c330cbe413114c7db0c6de454d0d02ee
Likhithraj19 <likhithrj@gmail.com>
2025-11-30 18:50:27 +0530

    Enhance REPL command handling to support 'echo' and 'type' commands with appropriate responses.

--- index.js
+++ index.js
@@ -14,7 +14,32 @@ function prompt(){
       return;
     }
 
-    console.log(`${answer}: command not found`);
+    if(answer.startsWith("echo ")){
+      console.log(`${answer.slice(5)}`);
+    }
+    else if(answer.startsWith("type ")){
+      const cmd = answer.slice(5).trim();
+      
+      switch (cmd) {
+        case "echo":
+          console.log("echo is a shell builtin");
+          break;
+
+        case "exit":
+          console.log("exit is a shell builtin");
+          break;
+        
+        case "type":
+          console.log("type is a shell builtin");
+          break;
+        
+        default:
+          console.log(`${cmd}: not found`);
+      }
+    }
+    else{
+      console.log(`${answer}: command not found`);
+    }
 
     prompt();
    
