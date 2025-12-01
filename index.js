dfc474a315199f2fefd150f0e5b9524d9667fd79
Likhithraj19 <likhithrj@gmail.com>
2025-12-01 12:14:06 +0530

    Added path

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const readline = require("readline");
+const path  = require("node:path");
+// const os = require("os");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// console.log("os.path", os.arch());
+
+// const pathName = path.join('/usr', 'bin');
+// console.log("pathName", pathName);
 
 function prompt(){
   rl.question("$ ", (answer) => {
@@ -19,7 +25,10 @@ function prompt(){
     }
     else if(answer.startsWith("type ")){
       const cmd = answer.slice(5).trim();
-      
+
+      const pathName = path.join('/usr', 'bin', `${cmd}`);
+      const validLocal = path.join('/usr', 'local', 'bin', `${cmd}`);
+
       switch (cmd) {
         case "echo":
           console.log("echo is a shell builtin");
@@ -31,6 +40,18 @@ function prompt(){
         
         case "type":
           console.log("type is a shell builtin");
+          break;
+         
+        case "grep":
+          console.log(`grep is ${pathName}`);
+          break;
+        
+        case "ls":
+          console.log(`ls is ${pathName}`);
+          break;
+          
+        case "valid_command":
+          console.log(`valid_command is ${validLocal}`);
           break;
         
         default:
